cd190a039f6d0d446c2c02f7b7f9ae3c9ea0e92d
Gediminas <g.jancys@gmc.lt>
2017-07-19 23:08:04 +0300

    Inicijuotas projektas

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=simplechatserver
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/simplechatserver
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=simplechatserver.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/simplechatserver.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=simplechatserver
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/simplechatserver
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=simplechatserver.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/simplechatserver.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
