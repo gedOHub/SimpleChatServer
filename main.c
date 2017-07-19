01dd965a7d9171e01f8c86448b3c01e64990be08
Gediminas <g.jancys@gmc.lt>
2017-07-19 23:59:08 +0300

    Nustatymu nuskaitymas is failo

--- main.c
+++ main.c
@@ -13,12 +13,52 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <libconfig.h>
 
+
+#define CONFIG_FILE "server.conf"
 /*
  * 
  */
 int main(int argc, char** argv) {
+    const char* ip;
+    int* port;
+    config_t *config = malloc(sizeof(config_t));
+    
+    // Inicijuoju nustatymu nuskaitymo objekta
+    config_init( config );
+    
+    // Nuskaitau nustatymu faila
+    if(! config_read_file(config, CONFIG_FILE))
+    {
+      fprintf(stderr, "%s:%d - %s\n", config_error_file(config),
+              config_error_line(config), config_error_text(config));
+      config_destroy(config);
+      return(EXIT_FAILURE);
+    }
+    
+    // Nuskaitau IP reiksme
+    if(!config_lookup_string(config, "IP", &ip)){
+        fprintf(stderr, "No 'IP' setting in configuration file.\n");
+        free(config);
+        return (EXIT_FAILURE);
+    }
+    
+    port = malloc(sizeof(int));
+    // Nuskaitau PORT reiksme
+    if(!config_lookup_int(config, "PORT", port)){
+        fprintf(stderr, "No 'PORT' setting in configuration file.\n");
+        
+        free(config);
+        free(port);
+        free((char *)ip);
+        
+        return (EXIT_FAILURE);
+    }
+    
+    printf("Server settings: %s:%d\n", ip, *port);
 
+    
     return (EXIT_SUCCESS);
 }
 
